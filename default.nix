3691b65035dd8d2c940715b4c686acd764a8c52e
Tom Cammann <tom.cammann@forgerock.com>
2023-08-07 14:48:15 +0100

    fix nix build on linux

--- default.nix
+++ default.nix
@@ -4,6 +4,17 @@ pkgs.rustPlatform.buildRustPackage {
   version = "0.1";
   cargoLock.lockFile = ./Cargo.lock;
   src = pkgs.lib.cleanSource ./.;
-  buildInputs = (if (pkgs.stdenv.isDarwin) then [ pkgs.openssl pkgs.darwin.apple_sdk.frameworks.Security ] else [ pkgs.openssl ]);
+  buildInputs = (
+    if (pkgs.stdenv.isDarwin) then
+      [
+        pkgs.openssl
+        pkgs.darwin.apple_sdk.frameworks.Security
+      ]
+    else
+      [
+        pkgs.openssl
+        pkgs.pkgconfig
+      ]
+  );
 }
 
